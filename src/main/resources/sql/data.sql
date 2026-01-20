ed0d68a6cc37d066e689d1f275aca7b7b4b940bb
Hiraina <hei.hiraina@gmail.com>
2026-01-20 21:47:39 +0300

    Update data.sql and add script sql for data insertion for dish_ingredient table

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -5,14 +5,16 @@ values (1, 'Salaide fraîche', 'STARTER'),
        (4, 'Gâteau au chocolat ', 'DESSERT'),
        (5, 'Salade de fruits', 'DESSERT');
 
+SELECT setval('dish_id_seq', (SELECT COALESCE(MAX(id), 0) FROM dish));
 
-insert into ingredient (id, name, category, price, id_dish)
-values (1, 'Laitue', 'VEGETABLE', 800.0, 1),
-       (2, 'Tomate', 'VEGETABLE', 600.0, 1),
-       (3, 'Poulet', 'ANIMAL', 4500.0, 2),
-       (4, 'Chocolat ', 'OTHER', 3000.0, 4),
-       (5, 'Beurre', 'DAIRY', 2500.0, 4);
+insert into ingredient (id, name, category, price)
+values (1, 'Laitue', 'VEGETABLE', 800.0),
+       (2, 'Tomate', 'VEGETABLE', 600.0),
+       (3, 'Poulet', 'ANIMAL', 4500.0),
+       (4, 'Chocolat ', 'OTHER', 3000.0),
+       (5, 'Beurre', 'DAIRY', 2500.0);
 
+SELECT setval('ingredient_id_seq', (SELECT COALESCE(MAX(id), 0) FROM ingredient));
 
 
 update dish
@@ -23,3 +25,13 @@ update dish
 set price = 6000.0
 where id = 2;
 
+
+-- data for dish_ingredient table
+insert into dish_ingredient (id, id_dish, id_ingredient, quantity_required, unit)
+values (1, 1, 1, 0.20, 'KG'),
+       (2, 1, 2, 0.15, 'KG'),
+       (3, 2, 3, 1.00, 'KG'),
+       (4, 4, 4, 0.30, 'KG'),
+       (5, 4, 5, 0.20, 'KG');
+
+select setval('dish_ingredient_id_seq', (SELECT COALESCE(MAX(id), 0) FROM dish_ingredient));
