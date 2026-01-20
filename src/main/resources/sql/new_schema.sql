8ee5006b683005fbb1b435896b22ced83ca6670a
Hiraina <hei.hiraina@gmail.com>
2026-01-20 21:08:32 +0300

    Add new_schema.sql

---
+++ src/main/resources/sql/new_schema.sql
@@ -0,0 +1,32 @@
+create type dish_type as enum ('STARTER', 'MAIN', 'DESSERT');
+
+
+create table dish
+(
+    id        serial primary key,
+    name      varchar(255),
+    dish_type dish_type,
+    price     numeric(10,2)
+);
+
+create type ingredient_category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+create table ingredient
+(
+    id       serial primary key,
+    name     varchar(255),
+    price    numeric(10, 2),
+    category ingredient_category
+);
+
+create type unit_type as enum ('PCS', 'KG', 'L');
+
+create table dishIngredient
+(
+    id                  serial primary key,
+    id_dish             int references dish(id),
+    id_ingredient       int references ingredient(id),
+    quantity_required   numeric(6,3),
+    unit                unit_type,
+    constraint unique_dish_ingredient unique (id_dish, id_ingredient)
+);
