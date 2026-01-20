804b583bf85d3f5d08c6de403deca1f00254c11c
Hiraina <hei.hiraina@gmail.com>
2026-01-20 21:03:30 +0300

    Add some privileges to mini_dish_db_manager

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -2,4 +2,14 @@ create database "mini_dish_db";
 
 create user "mini_dish_db_manager" with password '123456';
 
--- Grant all privileges
+\c "mini_dish_db"
+
+GRANT CONNECT ON DATABASE "mini_dish_db" TO "mini_dish_db_manager";
+
+GRANT USAGE, CREATE ON SCHEMA public TO "mini_dish_db_manager";
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON ALL TABLES IN SCHEMA public TO "mini_dish_db_manager";
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO "mini_dish_db_manager";
