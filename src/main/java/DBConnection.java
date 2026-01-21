688f75482bc06b92431e03f9e04edd8d30a96885
Hiraina <hei.hiraina@gmail.com>
2026-01-21 08:32:33 +0300

    Update DBConctexion class, use system environment variables

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -6,10 +6,10 @@ public class DBConnection {
 
     public Connection getConnection() {
         try {
-            String jdbcURl = System.getenv("JDBC_URl"); //
-            String user = System.getenv("USER"); //mini_dish_db_manager
-            String password = System.getenv("PASSWORD"); //123456
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/mini_dish_db", "postgres", "postgres");
+            String db_url = System.getenv("DB_URL");
+            String db_user = System.getenv("DB_USER");
+            String db_password = System.getenv("DB_PASSWORD");
+            return DriverManager.getConnection(db_url, db_user, db_password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
